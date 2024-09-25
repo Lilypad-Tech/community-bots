62189f2ee02b1b7ed71f0b3b1a5e0a0e74a7d196
Rodebrecht <42837438+Rodebrechtd@users.noreply.github.com>
2024-09-25 22:23:42 +0300

    Add POW Monitoring Discord Bot (#2)
    * Add POW Monitoring Discord Bot
    
    * Change Arbiscan api key placement
    
    * Add POW github action
    
    * Refactor variable name for OSS

--- vars.py
+++ vars.py
@@ -4,4 +4,15 @@ import os
 GIT_API_URL = "https://api.github.com/repos/Lilypad-Tech/lilypad/issues"
 
 # Discord webhook URL (you can set your actual webhook URL here)
-DISCORD_WEBHOOK = os.getenv('DISCORD_WEBHOOK')
+OSS_DISCORD_WEBHOOK = os.getenv('OSS_DISCORD_WEBHOOK')
+POW_MONITORING_WEBHOOK = os.getenv('POW_DISCORD_WEBHOOK')
+
+# Discord Roles
+LILYTEAM_DISCORD_ROLE = "<@&1212902935969669140>"
+LILYPAD_ADVOCATE_DISCORD_ROLE = "<@&1255696024161226783>"
+
+# Arbiscan
+ARBISCAN_API_KEY = os.getenv('ARBISCAN_API_KEY')
+ARBISCAN_API_ENDPOINT = f'https://api-sepolia.arbiscan.io/api'
+POW_CONTRACT_ADDRESS = "0x8b852ba45293d6dd51b10c57625c6c5f25adfb40"
+POW_CONTRACT_ADDRESS_URL = f"https://sepolia.arbiscan.io/address/{POW_CONTRACT_ADDRESS}"
