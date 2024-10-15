71d6ccc957945f7f72a9a4e73f0804f6413ec929
pbillingsby <45179070+PBillingsby@users.noreply.github.com>
2024-10-14 20:25:05 -0400

    fix: update logs_bot to use channel id not name

--- logs_bot.py
+++ logs_bot.py
@@ -31,8 +31,8 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    # If the message is in a thread under the 'i-need-help' forum channel and contains attachments
-    if isinstance(message.channel, discord.Thread) and message.channel.parent.name == '🙋🏻│i-need-help' and message.attachments:
+    # If the message is in a thread under the 'i-need-help' forum channel using ID and contains attachments
+    if isinstance(message.channel, discord.Thread) and message.channel.parent_id == 1230231823674642513 and message.attachments:
         for attachment in message.attachments:
             if attachment.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.avif', '.webp')):
                 try:
